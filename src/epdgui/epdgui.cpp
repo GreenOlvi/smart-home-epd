54e608fc9d9c35c71e3bd11258c93b8925b68fc7
Piotr Szulc <szulo123@gmail.com>
2023-09-30 12:54:56 +0200

    Adjusted typo to lib

--- src/epdgui/epdgui.cpp
+++ src/epdgui/epdgui.cpp
@@ -103,7 +103,7 @@ void EPDGUI_Run(Frame_Base* frame)
             return;
         }
 
-        if (M5.TP.avaliable())
+        if (M5.TP.available())
         {
             M5.TP.update();
             uint16_t is_finger_up = M5.TP.isFingerUp() ? 1 : 0;
